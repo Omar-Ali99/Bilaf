44d30a213e525392218a20ca7faf8042b6db1f95
Omar Ali <Omar.ali99@live.com>
2023-06-14 18:28:01 +0300

    PostgreSQL edited for all

--- Bilaf/main_app/views.py
+++ Bilaf/main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-from .models import User
+
 
 # Create your views here.
 def home_page(request: HttpRequest):
