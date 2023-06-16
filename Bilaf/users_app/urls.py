b4fed311ac76370193af9bbb1d12cc9c892b3433
MK <46850924+97MK@users.noreply.github.com>
2023-06-16 18:40:34 +0300

    Users: Login, Logout, SignUp
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * initial commit
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * modify on user stories
    
    * requirments added
    
    * Update README.md
    
    * Update README.md
    
    * change DB to PostgreSQL
    
    * Update README.md
    
    * Update requirments.txt
    
    * PostgreSQL edited for all
    
    * dont pull. just cheack
    
    * Integrate elephantsql DB
    
    * still need fix css
    
    * working header footer
    
    * add- pages
    
    * added login pages. and fix base and home page
    
    * Add Store Model
    
    * user:login, logout, signup
    
    ---------
    
    Co-authored-by: Omar Ali <101348008+Omar-Ali99@users.noreply.github.com>
    Co-authored-by: Omar Ali <Omar.ali99@live.com>
    Co-authored-by: amalsaab <441102683@student.ksu.edu.sa>
    Co-authored-by: 1947860@uj.edu.sa <1947860@uj.edu.sa>

--- Bilaf/users_app/urls.py
+++ Bilaf/users_app/urls.py
@@ -4,11 +4,8 @@ from . import views
 app_name = "users_app"
 
 urlpatterns = [
-    path('sign_up/', views.signup_page, name = 'signup_page'),
-
-    path('login/', views.login_page, name = 'login_page'),
-
-    path('signout/', views.signout_page, name = 'signout_page'),
-
-    path('no_permission_page/', views.no_permission_page, name = 'no_permission_page')
+    path("sign_up/", views.signup, name="signup"),
+    path("login/", views.login_page, name="login"),
+    path("signout/", views.signout_page, name="signout_page"),
+    path("no_permission_page/", views.no_permission_page, name="no_permission_page"),
 ]
