4f4879151e0f03be9abd4969b9e2dc104fd44bf9
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-06-17 18:09:09 +0300

    here

--- Bilaf/main_app/urls.py
+++ Bilaf/main_app/urls.py
@@ -6,7 +6,10 @@ app_name = "main_app"
 urlpatterns = [
     path('', views.home_page, name = 'home_page'),
     path('base/', views.base_page , name = 'base'),
-    path('add/', views.add_page , name = 'add_page')
+    path('add/', views.add_page , name = 'add_page'),
+    path('detail/', views.product_details , name = 'product_details'),
+    path('cart/', views.shoping_cart , name = 'shoping_cart'),
+    path('check_out/', views.check_out , name = 'check_out'),
+    path('contact/', views.contect_us , name = 'contact_us'),
 
-   
 ]
--- Bilaf/main_app/views.py
+++ Bilaf/main_app/views.py
@@ -16,4 +16,25 @@ def add_page(request: HttpRequest):
 
     return render(request, 'main_app/add_product.html')
 
+
+def check_out(request: HttpRequest):
+
+    return render(request, 'main_app/check_out.html')
+
+
+def product_details(request: HttpRequest):
+
+    return render(request, 'main_app/product_details.html')
+
+
+def shoping_cart(request: HttpRequest):
+
+    return render(request, 'main_app/shoping_cart.html')
+
+def contect_us(request: HttpRequest):
+
+    return render(request, 'main_app/contact_us.html')
+
+
+
     
