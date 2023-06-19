cd764c35b9a4734290eb4aee83d8ed70295dc797
1947860@uj.edu.sa <1947860@uj.edu.sa>
2023-06-19 22:57:07 +0300

    updates and delete_ products and catgory

--- Bilaf/main_app/urls.py
+++ Bilaf/main_app/urls.py
@@ -16,11 +16,15 @@ urlpatterns = [
     path("merchant/adding_products/", views.add_product, name="add_product"),
     path("merchant/adding_categories/", views.add_categories, name="add_categories"),
     path("product/", views.product_page, name="product_page"),
-    path('detail/<product_id>/', views.product_detail , name = 'product_detail'),
+    path("product/update/<product_id>/", views.update_product, name="update_product"),
+    path("product/delete/<product_id>/", views.delete_product, name="delete_product"),
+    path('product/detail/<product_id>/', views.product_detail , name = 'product_detail'),
     path("catgory/", views.catgory_page, name="catgory_page"),
+    path("catgory/update/<categories_id>/", views.update_catgory, name="update_catgory"),
+    path("catgory/delete/<categories_id>/", views.delete_product, name="delete_catgory"),
     path("store/", views.store_page, name="store_page"),
 
-
+    
 
 
 ]
